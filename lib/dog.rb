d749c795ad816780d3f0674238e35393c7c43f30
Damon Self <damongself@gmail.com>
2019-08-29 09:44:42 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# class Dog produces dog objects and contains methods to interact with dog objects created using Dog class
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
